32bb80a631847ca028da8da9147c74c4b15d8df1
rammuk <ram.mukmel@gmail.com>
2018-09-26 15:53:08 +0200

    activeOpacity

--- Lightbox.js
+++ Lightbox.js
@@ -129,6 +129,7 @@ export default class Lightbox extends Component {
           <TouchableHighlight
             underlayColor={this.props.underlayColor}
             onPress={this.open}
+            activeOpacity={1}
           >
             {this.props.children}
           </TouchableHighlight>
